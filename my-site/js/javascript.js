eb25bcb9cbaf87057d4c1de7fcbbb2c1b08d0fc7
alatimoreproducer <antoniolatimore.mail@gmail.com>
2021-08-03 14:28:42 -0700

    Initial file commit

---
+++ my-site/js/javascript.js
@@ -0,0 +1,33 @@
+
+function hidePage(){
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('focus').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
+hidePage();
+
+function showPage(){
+    document.querySelctor('.nav-link').addEventListener('click', style.display = 'block');
+}
+showPage();
+
+
+
+/*
+function showPage(){
+    document.getElementById('about').style.display = 'block';
+    document.getElementById('focus').style.display = 'block';
+    document.getElementById('contact').style.display = 'block';
+}
+
+
+
+function hidePage(){
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('focus').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
+hidePage();
+*/
+
+/*document.querySelector('.nav-link').style.display = 'none';*/
